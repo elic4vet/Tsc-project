18b88431412afd92295da968ae27e19a4228cc35
Elisabeth <elise.doisneau@gmail.com>
2024-04-12 20:14:45 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,20 @@
-let sales= 123_456_789;
+//let sales= 123_456_789;
 
-let course= "TypeScript";
+//let course= "TypeScript";
 
-let isPublished = true;
+//let isPublished = true;
 
-let level = 1;
+//let level = 1;
+
+const small = 1;
+const medium = 2;
+const large = 3;
+
+const enum Size {Small=1, Medium=2, Large=3}
+
+let size = Size.Medium;
+
+console.log(size);
+
+
 
